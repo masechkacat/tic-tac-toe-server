558f511eaf8103e3369e21da9deb380753c249a2
masechkacat <masechkacat@gmail.com>
2024-04-29 23:05:36 +0200

    some fix

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 3000;
   app.use(
     cors({
       origin: process.env.CORS_ORIGIN,
       credentials: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`Application is running on: http://0.0.0.0:${port}`);
+  });
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
